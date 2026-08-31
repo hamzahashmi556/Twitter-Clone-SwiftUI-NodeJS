e2ad219b7b592715ee876415dff0c4c07628cd38
Hamza <>
2026-08-31 09:03:53 +0500

    Notifications: added notification post & get router for user

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,8 @@ const express = require('express')
 
 const userRouter = require('./routers/user_router')
 const tweetRouter = require('./routers/tweet_router')
+const notificationRouter = require('./routers/notification_router')
+
 
 const app = express()
 
@@ -11,6 +13,7 @@ app.use(express.json())
 
 app.use(userRouter)
 app.use(tweetRouter);
+app.use(notificationRouter);
 
 const port = process.env.PORT || 3000
 
---
+++ backend/src/routers/notification_router.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const Notification = require('../models/notification')
+const auth = require('../middleware/auth')
+const multer = require('multer');
+const sharp = require('sharp');
+
+// Original Router
+const router = new express.Router()
+
+router.post('/notifications', auth, async (req, res) => {
+    try {
+        const notification = new Notification({
+            ...req.body,
+            user: req.user._id
+        })
+        await notification.save()
+        res.status(200).send(notification)
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+})
+
+router.get('/notification/:id', async (req, res) => {
+    try {
+        const id = req.params.id
+        const notifications = await Notification.find({ receiverId: id })
+        return res.status(200).send(notifications)
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+})
+
+module.exports = router
+
