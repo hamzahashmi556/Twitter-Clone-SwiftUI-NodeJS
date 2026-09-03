c8cd873e934dc8ff94d690ff155dd84b6833aa3f
Hamza.Alam <>
2026-09-03 14:11:43 +0500

    frontend: added xcode project

---
+++ frontend/Twitter Clone/Twitter_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_CloneApp.swift
+//  Twitter Clone
+//
+//  Created by PSG-MDU-HAMZA on 03/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct Twitter_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
